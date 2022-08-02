a685fcf06c4bda3aff1b124f817a00ec8c7ec6e2
Paulo <paulo.freire53@gmail.com>
2022-08-02 13:59:02 -0300

    adjust target with rel

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -18,19 +18,31 @@ export function Sidebar() {
       <footer>
         <div className={styles.socialLinks}>
           <LinkedinLogo size={32} color="#eba417" />
-          <a href="https://www.linkedin.com/in/phfreire/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/phfreire/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Linkedin
           </a>
         </div>
         <div className={styles.socialLinks}>
           <GithubLogo size={32} color="#eba417" />
-          <a href="https://github.com/paulohfreire" target="_blank">
+          <a
+            href="https://github.com/paulohfreire"
+            target="_blank"
+            rel="noreferrer"
+          >
             Github
           </a>
         </div>
         <div className={styles.socialLinks}>
           <TwitterLogo size={32} color="#eba417" />
-          <a href="https://twitter.com/freireopaulo" target="_blank">
+          <a
+            href="https://twitter.com/freireopaulo"
+            target="_blank"
+            rel="noreferrer"
+          >
             Twitter
           </a>
         </div>
